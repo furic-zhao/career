bc6dc513eb9dcaceb87b70edbfb76cc717f01c0a
zhaohuijian@360.cn <36550202@qq.com>
2016-07-09 23:28:34 +0800

    目录调整

--- project/src/views/index/index.js
+++ project/src/views/index/index.js
@@ -1,6 +1,5 @@
 var moduleNav = require('./modules/nav');
 
-<<<<<<< HEAD
 var moduleExperience = require('./modules/experience');
 
 var moduleWorks = require('./modules/works');
@@ -43,27 +42,3 @@ moduleWorks.render();
 关于本系统
  */
 moduleSystem.render();
-=======
-var moduleJingli = require('./modules/jingli');
-
-var moduleJingliWorks = require('./modules/jingli-works');
-
-var moduleWorks = require('./modules/works');
-
-moduleNav.render().then(function(data) {
-
-});
-
-moduleJingli.render().then(function(data) {
-
-});
-
-moduleJingliWorks.render().then(function(data) {
-
-});
-
-moduleWorks.render().then(function(data) {
-
-});
-
->>>>>>> 08d0c018126c5a36bb5ce6d9eef5b73e19a376f0
